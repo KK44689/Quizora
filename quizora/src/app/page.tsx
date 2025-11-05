c670c48790ace8a081bd07e91ad9e8c6f6844deb
Kirima Kangwanwongsakul <61814979+KK44689@users.noreply.github.com>
2025-11-05 16:59:32 +0700

    implement landing page

--- quizora/src/app/page.tsx
+++ quizora/src/app/page.tsx
@@ -1,65 +1,35 @@
 import Image from "next/image";
+import { Metadata } from "next";
+import { nunito_sans } from "./ui/font";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Quizora",
+}
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex flex-row md:flex-row sm:flex-col min-h-screen W-full items-center justify-center font-sans bg-[#1935CA]">
+      <div className="flex flex-col w-full items-center justify-center gap-8">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+          src='/quizora-white.png'
+          alt={"Quizora Logo"}
+          width={435}
+          height={89} />
+
+        <h1 className={`${nunito_sans} text-xl text-white md:text-3xl md:leading-normal font-bold`}>The ultimate quiz game for curious minds.</h1>
+      </div>
+
+      <div className="flex flex-col min-h-screen w-full max-w-3xl items-center justify-center gap-8 bg-white">
+        <p className={`${nunito_sans} text-xl md:text-3xl md:leading-normal text-[#1935CA] text-center`}>
+          <b>Used this info to try it yourself!</b><br /><br />
+          <b>Email:</b> test@quizora.com<br />
+          <b>Password:</b> thisisapassword<br />
+        </p>
+        <Link href="/dashboard">  {/*TODO: Now it's bypass to dashboard, changed to login when ready*/}
+          <button className="text-white bg-[#1935CA] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Login</button>
+        </Link>
+      </div>
     </div>
   );
 }
---
+++ quizora/src/app/ui/font.ts
@@ -0,0 +1,5 @@
+import { Nunito_Sans } from 'next/font/google';
+
+export const nunito_sans = Nunito_Sans({
+    weight: ['400', '700'],
+});
