4c4ca759ef3d44d6d1694cb5470429962da977a9
Kirima Kangwanwongsakul <61814979+KK44689@users.noreply.github.com>
2025-11-07 14:59:41 +0700

    adjust avatar size

--- quizora/src/app/ui/dashboard/avatar.tsx
+++ quizora/src/app/ui/dashboard/avatar.tsx
@@ -3,17 +3,17 @@ import { poppins } from "../font";
 
 export default function Avatar() {
     return (
-        <div className="flex gap-2 items-center ml-48">
+        <div className="flex gap-2 items-center ml-8 md:ml-48">
             {/* TODO: fetch image & name from server */}
             <Image
                 src="/profile-pic.png"
                 alt="avatar picture"
                 width={48}
                 height={48}
-                className="rounded-full"
+                className="w-8 h-8 md:w-12 md:h-12 object-cover rounded-full"
             />
 
-            <p className={`${poppins.className} text-[var(--theme-grey)]`}>Luna Duck</p>
+            <p className={`${poppins.className} hidden md:block text-[var(--theme-grey)]`}>Luna Duck</p>
         </div>
     );
 }
