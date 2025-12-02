94a3963af46b46360cde76d4bf62dec71eb07b80
Kirima Kangwanwongsakul <61814979+KK44689@users.noreply.github.com>
2025-12-02 16:17:25 +0700

    implement quiz confirm review panel

--- src/app/ui/quiz/quiz-confirm-review.tsx
+++ src/app/ui/quiz/quiz-confirm-review.tsx
@@ -1,4 +1,7 @@
 import { useRouter } from "next/navigation";
+import QuizPassedIcon from "@/app/assets/icons/passed-icon.svg";
+import QuizRetryIcon from "@/app/assets/icons/retry-icon.svg";
+import { poppins } from "../font";
 
 export function QuizConfirmReviewPanel({
   score,
@@ -16,19 +19,20 @@ export function QuizConfirmReviewPanel({
   }
 
   return (
-    <div className="bg-white rounded-lg p-10 md:w-119 max-w-2xl md:h-109 shadow-lg relative flex flex-col items-center justify-center gap-9">
-      <h1>{isPassed ? "Congratulations you have passed" : "Almost there! Try again!"}</h1>
-      <p>You scored {score * 10}%</p>
+    <div className={`${poppins.className} bg-white rounded-4xl p-10 scale-90 md:scale-100 md:w-119 max-w-2xl md:h-109 shadow-lg relative flex flex-col items-center justify-center gap-9`}>
+      <h1>{isPassed ? <QuizPassedIcon className="w-25 h-25" /> : <QuizRetryIcon className="w-25 h-25" />}</h1>
+      <h1 className="font-bold text-xl">{isPassed ? "Congratulations you have passed" : "Almost there! Try again!"}</h1>
+      <p className="text-base">You scored {score * 10}%</p>
       <div className="flex flex-row gap-8">
         <button
-          className=""
+          className="px-8 py-5 text-[var(--theme-blue)] text-base md:text-xl font-semibold rounded-lg border border-s border-[var(--theme-blue)]"
           onClick={onReview}
         >
           Review Quiz
         </button>
 
         <button
-          className=""
+          className="px-8 py-5 bg-[var(--theme-blue)] text-white text-base md:text-xl font-semibold rounded-lg"
           onClick={onBack}
         >
           Go Home
